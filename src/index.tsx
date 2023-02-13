1b9dcd58d6f856d7e395ce434fbfbd060a999dcf
Mark de Vries <mark@lamalama.nl>
2023-02-13 20:22:55 +0100

    setup extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,94 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { ActionPanel, List, Action, getPreferenceValues, LaunchType } from "@raycast/api";
+import fetch from "node-fetch";
+
+type Projects = {
+  project: string;
+  html_url: string;
+  projects: Project[];
+};
+
+type Workspaces = {
+  project: string;
+  html_url: string;
+  workspaces: Workspace[];
+};
+
+type Project = {
+  url: string;
+  html_url: string;
+  name: string;
+  display_name: string;
+  status: string;
+};
+
+type Workspace = {
+  url: string;
+  html_url: string;
+  id: number;
+  name: string;
+  domain: string;
+};
 
 export default function Command() {
+  const [projects, setProjects] = useState([]);
+  const [workspaces, setWorkspaces] = useState([]);
+  const [workspace, setWorkspace] = useState("");
+
+  const { ACCESS_TOKEN } = getPreferenceValues();
+
+  useEffect(() => {
+    fetch("https://api.buddy.works/workspaces", {
+      headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setWorkspaces((data as any).workspaces);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (workspace === "") {
+      return;
+    }
+
+    fetch(`https://api.buddy.works/workspaces/${workspace}/projects`, {
+      headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProjects((data as any).projects);
+      });
+  }, [workspace]);
+
   return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+    <List
+      searchBarAccessory={
+        <List.Dropdown
+          tooltip="Workspaces"
+          onChange={(workspace) => {
+            setWorkspace(workspace);
+          }}
+        >
+          {(workspaces as Workspace[]).map((workspace, index) => {
+            return <List.Dropdown.Item key={`workspace-${index}`} title={workspace.name} value={workspace.domain} />;
+          })}
+        </List.Dropdown>
+      }
+    >
+      {(projects as Project[]).map((project, index) => {
+        return (
+          <List.Item
+            key={`project-${index}`}
+            title={project.name}
+            actions={
+              <ActionPanel title="Buddy">
+                <Action.OpenInBrowser url={`${project.html_url}/pipelines`} />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
     </List>
   );
 }
